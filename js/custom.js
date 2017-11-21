d8551d3a17f67fa532c589234cda4338485d7a4c
Sudam Regmi <sudam2ins@gmail.com>
2017-11-21 16:03:54 +0545

    first-site

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.toggle a').click(function(e){
+		$('.mynav').addClass('show-menu');
+		$('.body-inactive').fadeIn(250);
+		e.preventDefault();
+	});
+	$('.body-inactive, .close-menu').click(function(e){
+		$('.mynav').removeClass('show-menu');
+		$('.body-inactive').fadeOut(250);
+		e.preventDefault();
+	});
+})
